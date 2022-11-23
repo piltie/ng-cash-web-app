c4048187e52dea37d60ca3ca7dd1e41316bfe35b
piltie <95034183+piltie@users.noreply.github.com>
2022-11-22 21:43:48 -0300

    acho q eh isos ne

--- backend/config/default.ts
+++ backend/config/default.ts
@@ -1,7 +1,7 @@
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASSWORD
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
 
 export default {
   port: 3000,
-  dbUri: `postgres://${dbUser}:${dbPassword}@localhost:5432/ngcashweb`
+  dbUri: `postgres://postgres:1234@db:5432/ngcashweb`,
 };
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 8080,
+  },
   plugins: [react()],
 });
